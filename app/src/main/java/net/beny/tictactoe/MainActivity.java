1d05d8a0b294e94e411559175cd826589b08e8dc
Rafał Ratajski <rratajski@gmail.com>
2015-02-24 22:21:21 +0100

    Dodanie przycisku info, usprawnienie metod komunikatu, bugfix na czyszczenie planszy.

--- app/src/main/java/net/beny/tictactoe/MainActivity.java
+++ app/src/main/java/net/beny/tictactoe/MainActivity.java
@@ -15,7 +15,6 @@ public class MainActivity extends ActionBarActivity {
     //główne zmienne gry
     int ruch = 1;
     String[][] poleGry = new String[3][3];
-    Boolean czyKoniec = false;
     String ktoWygral;
 
     @Override
@@ -24,30 +23,34 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-    //okno komunikatu wyniku gry
-    public class Komunikat extends DialogFragment {
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            builder.setMessage(ktoWygral)
-                    .setTitle(R.string.koniec_gry)
-                    .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            resetGry();
-                        }
-                    });
-            return builder.create();
-        }
+    //metoda komunikatu
+    public void komunikat() {
+        new AlertDialog.Builder(this)
+                .setMessage(ktoWygral)
+                .setTitle(R.string.koniec_gry)
+                .setPositiveButton(R.string.ok, null)
+                .setNegativeButton(R.string.info, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        komunikat_info();
+                    }
+                })
+                .show();
     }
 
-    //metoda pokazująca komunikat
-    public void komunikat() {
-        DialogFragment newFragment = new Komunikat();
-        newFragment.show(getSupportFragmentManager(), "oknoKomunikatu");
+    //metoda komunikatu info
+    public void komunikat_info() {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.infoTitle)
+                .setMessage(R.string.infoLine)
+                .setPositiveButton(R.string.ok, null)
+                .show();
     }
 
     //metoda do czyszczenia i resetowania gry
     public void resetGry() {
+        //komunikat wyniku
+        komunikat();
+
         //zmienne metody
         int[] buttonsId = new int[9];
         buttonsId[0] = R.id.button_0x0;
@@ -59,8 +62,6 @@ public class MainActivity extends ActionBarActivity {
         buttonsId[6] = R.id.button_2x0;
         buttonsId[7] = R.id.button_2x1;
         buttonsId[8] = R.id.button_2x2;
-
-        czyKoniec = false;
         ruch = 1;
 
         //czyszczenie tablicy poleGry
@@ -81,108 +82,103 @@ public class MainActivity extends ActionBarActivity {
 
     //obsługa kliknięcia i główna logika gry
     public void klik(View view) {
-        if (!czyKoniec) {
 
-            //używane metody
-            Button button = (Button) view;
-            int pozycjaX = 0;
-            int pozycjaY = 0;
 
-            // przypisanie pozycji przycisku do pola Gry.
-            switch (button.getId()) {
-                case R.id.button_0x0:
-                    pozycjaX = 0;
-                    pozycjaY = 0;
-                    break;
-                case R.id.button_0x1:
-                    pozycjaX = 0;
-                    pozycjaY = 1;
-                    break;
-                case R.id.button_0x2:
-                    pozycjaX = 0;
-                    pozycjaY = 2;
-                    break;
-                case R.id.button_1x0:
-                    pozycjaX = 1;
-                    pozycjaY = 0;
-                    break;
-                case R.id.button_1x1:
-                    pozycjaX = 1;
-                    pozycjaY = 1;
-                    break;
-                case R.id.button_1x2:
-                    pozycjaX = 1;
-                    pozycjaY = 2;
-                    break;
-                case R.id.button_2x0:
-                    pozycjaX = 2;
-                    pozycjaY = 0;
-                    break;
-                case R.id.button_2x1:
-                    pozycjaX = 2;
-                    pozycjaY = 1;
-                    break;
-                case R.id.button_2x2:
-                    pozycjaX = 2;
-                    pozycjaY = 2;
-                    break;
-            }
+        //używane metody
+        Button button = (Button) view;
+        int pozycjaX = 0;
+        int pozycjaY = 0;
 
-            // przypisanie wartości do ruchu i pola gry
-            if (ruch % 2 != 0) {
-                button.setText(R.string.x);
-                button.setEnabled(false);
-                poleGry[pozycjaX][pozycjaY] = "X";
-                ruch++;
-            } else {
-                button.setText(R.string.o);
-                button.setEnabled(false);
-                poleGry[pozycjaX][pozycjaY] = "O";
-                ruch++;
-            }
+        // przypisanie pozycji przycisku do pola Gry.
+        switch (button.getId()) {
+            case R.id.button_0x0:
+                pozycjaX = 0;
+                pozycjaY = 0;
+                break;
+            case R.id.button_0x1:
+                pozycjaX = 0;
+                pozycjaY = 1;
+                break;
+            case R.id.button_0x2:
+                pozycjaX = 0;
+                pozycjaY = 2;
+                break;
+            case R.id.button_1x0:
+                pozycjaX = 1;
+                pozycjaY = 0;
+                break;
+            case R.id.button_1x1:
+                pozycjaX = 1;
+                pozycjaY = 1;
+                break;
+            case R.id.button_1x2:
+                pozycjaX = 1;
+                pozycjaY = 2;
+                break;
+            case R.id.button_2x0:
+                pozycjaX = 2;
+                pozycjaY = 0;
+                break;
+            case R.id.button_2x1:
+                pozycjaX = 2;
+                pozycjaY = 1;
+                break;
+            case R.id.button_2x2:
+                pozycjaX = 2;
+                pozycjaY = 2;
+                break;
+        }
 
-            //wykrywanie wygranej
-            String[] wybor = new String[2];
-            wybor[0] = "X";
-            wybor[1] = "O";
+        // przypisanie wartości do ruchu i pola gry
+        if (ruch % 2 != 0) {
+            button.setText(R.string.x);
+            button.setEnabled(false);
+            poleGry[pozycjaX][pozycjaY] = "X";
+            ruch++;
+        } else {
+            button.setText(R.string.o);
+            button.setEnabled(false);
+            poleGry[pozycjaX][pozycjaY] = "O";
+            ruch++;
+        }
 
-            for (int w = 0; w < 2; w++) {
-                for (int x = 0; x < 3; x++) {
-                    if ((poleGry[x][0] == wybor[w]) & (poleGry[x][1] == wybor[w]) &
-                            (poleGry[x][2] == wybor[w])) {
-                        czyKoniec = true;
-                        ktoWygral = "Wygrywa " + wybor[w];
-                        komunikat();
-                    }
-                }
-                for (int y = 0; y < 3; y++) {
-                    if ((poleGry[0][y] == wybor[w]) & (poleGry[1][y] == wybor[w]) &
-                            (poleGry[2][y] == wybor[w])) {
-                        czyKoniec = true;
-                        ktoWygral = "Wygrywa " + wybor[w];
-                        komunikat();
-                    }
-                }
-                if ((poleGry[0][0] == wybor[w]) & (poleGry[1][1] == wybor[w]) &
-                        (poleGry[2][2] == wybor[w])) {
-                    czyKoniec = true;
+        //wykrywanie wygranej
+        String[] wybor = new String[2];
+        wybor[0] = "X";
+        wybor[1] = "O";
+
+        for (int w = 0; w < 2; w++) {
+            for (int x = 0; x < 3; x++) {
+                if ((poleGry[x][0] == wybor[w]) & (poleGry[x][1] == wybor[w]) &
+                        (poleGry[x][2] == wybor[w])) {
                     ktoWygral = "Wygrywa " + wybor[w];
-                    komunikat();
-                }
-                if ((poleGry[0][2] == wybor[w]) & (poleGry[1][1] == wybor[w]) &
-                        (poleGry[2][0] == wybor[w])) {
-                    czyKoniec = true;
-                    ktoWygral = "Wygrywa " + wybor[w];
-                    komunikat();
+                    resetGry();
                 }
             }
-
-            if ((ruch == 10) & (!czyKoniec)) {
-                czyKoniec = true;
-                ktoWygral = "REMIS!";
-                komunikat();
+            for (int y = 0; y < 3; y++) {
+                if ((poleGry[0][y] == wybor[w]) & (poleGry[1][y] == wybor[w]) &
+                        (poleGry[2][y] == wybor[w])) {
+                    ktoWygral = "Wygrywa " + wybor[w];
+                    resetGry();
+                }
+            }
+            if ((poleGry[0][0] == wybor[w]) & (poleGry[1][1] == wybor[w]) &
+                    (poleGry[2][2] == wybor[w])) {
+                ktoWygral = "Wygrywa " + wybor[w];
+                resetGry();
+            }
+            if ((poleGry[0][2] == wybor[w]) & (poleGry[1][1] == wybor[w]) &
+                    (poleGry[2][0] == wybor[w])) {
+                ktoWygral = "Wygrywa " + wybor[w];
+                resetGry();
             }
         }
+
+        if (ruch == 10) {
+            ktoWygral = "REMIS!";
+            resetGry();
+        }
+
     }
 
 }
